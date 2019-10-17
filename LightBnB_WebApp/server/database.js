157cbc862a24c6bba2446260e04d43ed680bcf10
Lloyd <lloyd.gerry@gmail.com>
2019-10-17 21:30:55 +0000

    adding get my reservations as query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -81,9 +81,21 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
+
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  let now = now();
+  return pool.query(`
+  SELECT * FROM reservations
+  WHERE guest_id = $1 AND start_date IS NOT ${now}
+  LIMIT $2
+  `, [guest_id, limit])
+  .then(res => res.rows);
 }
+
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
